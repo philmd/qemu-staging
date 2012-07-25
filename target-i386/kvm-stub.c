ced680fca9dbf1047b288af11641d015d2a4cca7
Peter Maydell <peter.maydell@linaro.org>
2012-07-25 13:41:39 +0100

    kvm: Move kvm_allows_irq0_override() to target-i386
    kvm_allows_irq0_override() is a totally x86 specific concept:
    move it to the target-specific source file where it belongs.
    This means we need a new header file for the prototype:
    kvm_i386.h, in line with the existing kvm_ppc.h.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

---
+++ target-i386/kvm-stub.c
@@ -0,0 +1,17 @@
+/*
+ * QEMU KVM x86 specific function stubs
+ *
+ * Copyright Linaro Limited 2012
+ *
+ * Author: Peter Maydell <peter.maydell@linaro.org>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
+#include "kvm_i386.h"
+
+int kvm_allows_irq0_override(void)
+{
+    return 1;
+}

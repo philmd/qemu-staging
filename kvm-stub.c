21004dfb9b9033590c21409746d529d883a18a8d
Peter Maydell <peter.maydell@linaro.org>
2012-07-25 13:41:40 +0100

    kvm: Don't assume irqchip-in-kernel implies irqfds
    Instead of assuming that we can use irqfds if and only if
    kvm_irqchip_in_kernel(), add a bool to the KVMState which
    indicates this, and is set only on x86 and only if the
    irqchip is in the kernel.
    
    The kernel documentation implies that the only thing
    you need to use KVM_IRQFD is that KVM_CAP_IRQFD is
    advertised, but this seems to be untrue. In particular
    the kernel does not (alas) return a sensible error if you
    try to set up an irqfd when you haven't created an irqchip.
    If it did we could remove all this nonsense and let the
    kernel return the error code.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- kvm-stub.c
+++ kvm-stub.c
@@ -20,6 +20,7 @@
 KVMState *kvm_state;
 bool kvm_kernel_irqchip;
 bool kvm_async_interrupt_injection;
+bool kvm_irqfds_allowed;
 
 int kvm_init_vcpu(CPUArchState *env)
 {
--- kvm.h
+++ kvm.h
@@ -25,6 +25,7 @@
 extern int kvm_allowed;
 extern bool kvm_kernel_irqchip;
 extern bool kvm_async_interrupt_injection;
+extern bool kvm_irqfds_allowed;
 
 #if defined CONFIG_KVM || !defined NEED_CPU_H
 #define kvm_enabled()           (kvm_allowed)
@@ -38,10 +39,19 @@ extern bool kvm_async_interrupt_injection;
  * (where the vcpu must be stopped at a suitable point first).
  */
 #define kvm_async_interrupt_injection() (kvm_async_interrupt_injection)
+/**
+ * kvm_irqfds_enabled:
+ *
+ * Returns: true if we can use irqfds to inject interrupts into
+ * a KVM CPU (ie the kernel supports irqfds and we are running
+ * with a configuration where it is meaningful to use them).
+ */
+#define kvm_irqfds_enabled() (kvm_irqfds_allowed)
 #else
 #define kvm_enabled()           (0)
 #define kvm_irqchip_in_kernel() (false)
 #define kvm_async_interrupt_injection() (false)
+#define kvm_irqfds_enabled() (false)
 #endif
 
 struct kvm_run;

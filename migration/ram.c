323fcf192d691579a8a8b2ee73b1cd9f5a2566b4
Peter Maydell <peter.maydell@linaro.org>
2016-01-12 11:43:48 +0000

    fpu: Replace uint8 typedef with uint8_t
    Replace the uint8 softfloat-specific typedef with uint8_t.
    This change was made with
    
    find include hw fpu target-* -name '*.[ch]' | xargs sed -i -e 's/\buint8\b/uint8_t/g'
    
    together with manual removal of the typedef definition and
    manual fixing of more erroneous uses found via test compilation.
    
    It turns out that the only code using this type is an accidental
    use where uint8_t was intended anyway...
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- migration/ram.c
+++ migration/ram.c
@@ -264,7 +264,7 @@ struct DecompressParam {
     QemuMutex mutex;
     QemuCond cond;
     void *des;
-    uint8 *compbuf;
+    uint8_t *compbuf;
     int len;
 };
 typedef struct DecompressParam DecompressParam;

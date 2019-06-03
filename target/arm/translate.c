88bf74b96668df409927eba7dd1935ec13238c08
Peter Maydell <peter.maydell@linaro.org>
2019-06-03 16:16:56 +0100

    target/arm: Convert VNEG to decodetree
    Convert the VNEG instruction to decodetree.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- target/arm/translate.c
+++ target/arm/translate.c
@@ -3088,7 +3088,7 @@ static int disas_vfp_insn(DisasContext *s, uint32_t insn)
                 return 1;
             case 15:
                 switch (rn) {
-                case 1:
+                case 1 ... 2:
                     /* Already handled by decodetree */
                     return 1;
                 default:
@@ -3102,7 +3102,6 @@ static int disas_vfp_insn(DisasContext *s, uint32_t insn)
                 /* rn is opcode, encoded as per VFP_SREG_N. */
                 switch (rn) {
                 case 0x00: /* vmov */
-                case 0x02: /* vneg */
                 case 0x03: /* vsqrt */
                     break;
 
@@ -3280,9 +3279,6 @@ static int disas_vfp_insn(DisasContext *s, uint32_t insn)
                     switch (rn) {
                     case 0: /* cpy */
                         /* no-op */
-                        break;
-                    case 2: /* neg */
-                        gen_vfp_neg(dp);
                         break;
                     case 3: /* sqrt */
                         gen_vfp_sqrt(dp);
